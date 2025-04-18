ea7c82e5fb168f2fffc373257ef9bdc960f71421
laihongphuc <phuckstnk63dktd@gmail.com>
2025-04-18 15:03:46 +0700

    Add LoraPro adamW

--- lora_pro/adamw.py
+++ lora_pro/adamw.py
@@ -1,6 +1,6 @@
 import math
 import random
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union, Dict, List, Iterable
 
 import numpy as np
 import torch
@@ -25,18 +25,19 @@ class AdamW:
         self.beta1, self.beta2 = betas
         self.weight_decay = weight_decay
         self.eps = eps
-        self.model_params = list(model_params)
-        self.n_steps = 0
+        self.model_params = [param for param in model_params if param.requires_grad]
+        self.n_warmup_steps = 0
+        self.global_steps = 0
         self.L2_regularization = L2_regularization
         self.T_max = T_max
-        self.avg_grads = [torch.zeros_like(p.data) for p in self.model_params]
-        self.avg_sq_grads = [torch.zeros_like(p.data) for p in self.model_params]
+        self.avg_grads = {}
+        self.avg_sq_grads = {}
     
     def set_warmup(self):
-        if self.n_steps == self.T_max:
+        if self.n_warmup_steps == self.T_max:
             self.T_max *= 2
-            self.n_steps = 0
-        schedule_mul = 0.5 + 0.5 * np.cos(self.n_steps / self.T_max * np.pi)
+            self.n_warmup_steps = 0
+        schedule_mul = 0.5 + 0.5 * np.cos(self.n_warmup_steps / self.T_max * np.pi)
         self.lr = self.original_lr * schedule_mul
 
         
@@ -53,8 +54,15 @@ class AdamW:
     @torch.no_grad()
     def step(self):
         self.set_warmup()
-        self.n_steps += 1
-        for param, avg_grad, avg_sq_grad in zip(self.model_params, self.avg_grads, self.avg_sq_grads):
+        self.n_warmup_steps += 1
+        self.global_steps += 1
+        # for param, avg_grad, avg_sq_grad in zip(self.model_params, self.avg_grads, self.avg_sq_grads):
+        for idx, param in enumerate(self.model_params):
+            if self.global_steps == 1:
+                self.avg_grads[idx] = torch.zeros_like(param.data)
+                self.avg_sq_grads[idx] = torch.zeros_like(param.data)
+            avg_grad = self.avg_grads[idx]
+            avg_sq_grad = self.avg_sq_grads[idx]
             grad = param.grad
             # apply L2 regularization
             if self.L2_regularization:
@@ -62,13 +70,145 @@ class AdamW:
             # update moving averages
             avg_grad.mul_(self.beta1).add_(grad, alpha=1 - self.beta1)
             avg_sq_grad.mul_(self.beta2).add_(grad**2, alpha=1 - self.beta2)
+
             # compute bias-corrected moments
-            bias_correction1 = 1 - self.beta1 ** self.n_steps
-            bias_correction2 = 1 - self.beta2 ** self.n_steps
+            bias_correction1 = 1 - self.beta1 ** self.global_steps
+            bias_correction2 = 1 - self.beta2 ** self.global_steps
             avg_grad_corr = avg_grad / bias_correction1
             avg_sq_grad_corr = avg_sq_grad / bias_correction2
             
             # apply AdamW update
-            std = (avg_sq_grad_corr.sqrt() + self.eps).sqrt()
-            param.sub_(self.lr * avg_grad_corr / std + self.weight_decay * param.data)
+            std = (avg_sq_grad_corr.sqrt() + self.eps)
+            param.sub_(self.lr * (avg_grad_corr / std + self.weight_decay * param.data))
+
+def solve_sylvester(A, B, C, X=None):
+    ''' From the answer here: 
+        https://stackoverflow.com/questions/73713072/solving-sylvester-equations-in-pytorch
+    '''
+    if A.dtype is torch.bfloat16:
+        A = A.to(torch.float32)
+        B = B.to(torch.float32)
+        C = C.to(torch.float32)
+    B = -B
+    m = B.shape[-1];
+    n = A.shape[-1];
+    try:
+        R, U = torch.linalg.eig(A)
+    except:
+        print(A)
+    S, V = torch.linalg.eig(B)
+    F = torch.linalg.solve(U, (C + 0j) @ V)
+    W = R[..., :, None] - S[..., None, :]
+    Y = F / W
+    X = U[...,:n,:n] @ Y[...,:n,:m] @ torch.linalg.inv(V)[...,:m,:m]
+    return X.real if all(torch.isreal(x.flatten()[0]) 
+                for x in [A, B, C]) else X
+class AdamWLoraPro:
+    def __init__(
+        self,
+        model_params: Iterable[torch.nn.Parameter],
+        lr: float = 1e-3,
+        betas: Tuple[float, float] = (0.9, 0.999),
+        weight_decay: float = 0.01,
+        eps: float = 1e-8,
+        T_max: int = 1000,
+        L2_regularization: bool = False,
+        scaling_factor: float = 2.0,
+    ):
+        self.lr = lr
+        self.original_lr = lr
+        self.beta1, self.beta2 = betas
+        self.weight_decay = weight_decay
+        self.eps = eps
+        self.model_params = [param for param in model_params if param.requires_grad]
+        self.n_warmup_steps = 0
+        self.global_steps = 0
+        self.L2_regularization = L2_regularization
+        self.T_max = T_max
+        self.scaling_factor = scaling_factor
+        self.avg_grads = {}
+        self.avg_sq_grads = {}
+    
+    def set_warmup(self):
+        if self.n_warmup_steps == self.T_max:
+            self.T_max *= 2
+            self.n_warmup_steps = 0
+        schedule_mul = 0.5 + 0.5 * np.cos(self.n_warmup_steps / self.T_max * np.pi)
+        self.lr = self.original_lr * schedule_mul
+
+        
+
+
+    def zero_grad(self, set_to_none: bool = False):
+        for p in self.model_params:
+            if p.grad is not None:
+                if set_to_none:
+                    p.grad = None
+                else:
+                    p.grad.data.zero_()
+    
+    @torch.no_grad()
+    def step(self):
+        self.set_warmup()
+        self.n_warmup_steps += 1
+        self.global_steps += 1
+        for idx, (lora_A, lora_B) in enumerate(zip(self.model_params[::2], self.model_params[1::2])):
+            A = lora_A.data
+            B = lora_B.data
+            grad_A_origin = lora_A.grad
+            grad_B_origin = lora_B.grad
+            AA_T = A @ A.T 
+            B_TB = B.T @ B 
+            if self.global_steps == 1:
+                # for lora grad
+                AA_T_inv = torch.linalg.pinv(AA_T + self.eps * torch.eye(A.shape[0], device=grad_A_origin.device))
+                AA_T_inv = AA_T_inv.to(grad_A_origin.dtype)
+                grad_A = grad_A_origin
+                grad_B = (1 / self.scaling_factor ** 2) * grad_B_origin @ AA_T_inv
+            else:
+                AA_T_inv = torch.linalg.pinv(AA_T + self.eps * torch.eye(A.shape[0], device=grad_A_origin.device)) 
+                B_TB_inv = torch.linalg.pinv(B_TB + self.eps * torch.eye(A.shape[0], device=grad_A_origin.device)) 
+                AA_T_inv = AA_T_inv.to(A.dtype)
+                B_TB_inv = B_TB_inv.to(A.dtype)
+                grad_A = (1 / self.scaling_factor ** 2) * B_TB_inv @ grad_A_origin
+                grad_B = (1 / self.scaling_factor ** 2) * ((torch.eye(B.shape[0], device=grad_A_origin.device, dtype=A.dtype) - B @ B_TB_inv @ B.T) @ grad_B_origin @ AA_T_inv)   
+            equiv_grad = self.scaling_factor * B @ grad_A + self.scaling_factor * grad_B @ A
+            if self.global_steps == 1:
+                self.avg_grads[idx] = torch.zeros_like(equiv_grad)
+                self.avg_sq_grads[idx] = torch.zeros_like(equiv_grad)
+            avg_grad = self.avg_grads[idx]
+            avg_sq_grad = self.avg_sq_grads[idx]
+
             
+            avg_grad.mul_(self.beta1).add_(equiv_grad, alpha=1 - self.beta1)
+            avg_sq_grad.mul_(self.beta2).add_(equiv_grad**2, alpha=1 - self.beta2)
+            # compute bias-corrected moments
+            bias_correction1 = 1 - self.beta1 ** self.global_steps
+            bias_correction2 = 1 - self.beta2 ** self.global_steps
+            avg_grad_corr = avg_grad / bias_correction1
+            avg_sq_grad_corr = avg_sq_grad / bias_correction2
+            
+            # apply AdamW update
+            std = (avg_sq_grad_corr.sqrt() + self.eps)
+            g = (avg_grad_corr / std)
+            g = g.to(grad_A.dtype)
+
+            grad_A_orin_ = self.scaling_factor * B.T @ g 
+            grad_B_orin_ = self.scaling_factor * g @ A.T 
+
+            grad_A_origin.data = grad_A_orin_
+            grad_B_origin.data = grad_B_orin_
+
+            if self.global_steps == 1:
+                grad_A = grad_A_orin_ 
+                grad_B = (1 / self.scaling_factor ** 2) * grad_B_orin_ @ AA_T_inv 
+            else:
+                X = solve_sylvester(B.T @ B, A @ A.T, -(1 / self.scaling_factor ** 2) * B_TB_inv @ grad_A_orin_ @ A.T)
+                # X = torch.tensor(X, device=grad_A_origin.device, dtype=B.dtype)
+                X = X.to(device=grad_A_origin.device, dtype=B.dtype)
+
+                grad_A = (1 / self.scaling_factor ** 2) * B_TB_inv @ grad_A_orin_ + X @ A
+                grad_B = (1 / self.scaling_factor ** 2) * ((torch.eye(B.shape[0], device=grad_A_origin.device, dtype=A.dtype) - B @ B_TB_inv @ B.T) @ grad_B_orin_ @ AA_T_inv) - B @ X  
+
+            lora_A.sub_(self.lr * (grad_A + self.weight_decay * lora_A.data))
+            lora_B.sub_(self.lr * (grad_B + self.weight_decay * lora_B.data))
--- test/test_adamw.py
+++ test/test_adamw.py
@@ -83,3 +83,58 @@ def test_warmup():
             # print(optimizer.T_max)
     assert True
 
+def test_lora_pro():
+    # create a simple model
+    class SimpleModel(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.fc1 = nn.Linear(10, 10)
+            self.fc2 = nn.Linear(10, 1)
+
+        def forward(self, x):
+            x = self.fc1(x)
+            x = self.fc2(x)
+            return x
+        
+    # create a simple model
+    model = SimpleModel()
+    # create a lora model
+    from peft import LoraConfig, get_peft_model
+    lora_config = LoraConfig(
+        r=8,
+        lora_alpha=16,
+        lora_dropout=0.1,
+        target_modules=["fc1"]
+    )
+    model = get_peft_model(model, lora_config)
+    # create a simple optimizer
+    optimizer = adamw.AdamWLoraPro(model.parameters(), lr=1, T_max=1, weight_decay=0.01)
+    # create a simple loss function
+    criterion = nn.MSELoss()    
+    # create a simple input
+    x = torch.randn(10)
+    # forward pass
+    y = model(x)
+    # compute loss
+    loss = criterion(y, torch.randn(1))
+    # backward pass
+    loss.backward()
+    for i in range(10):
+        # step optimizer
+        optimizer.step()
+        if i == 0:
+            assert optimizer.lr == 1.
+            # n = 0, T = 1
+        elif i == 1:
+            assert optimizer.lr == 1.
+            # n = 1, T = 1 => n = 0, T = 2
+            # print(optimizer.T_max)
+        elif i == 7:
+            assert optimizer.lr == 1.
+            # n = 2, T = 2 => n = 0, T = 4
+            # print(optimizer.T_max)
+    assert True
+
+
+if __name__ == "__main__":
+    test_extract_lora_params()
